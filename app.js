54e825135b7437f04ac3be941244ba52a3245224
hrushikabhaskargopale <hrushika.bhaskar@gmail.com>
2025-04-16 14:41:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello from Docker!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`);
+});
